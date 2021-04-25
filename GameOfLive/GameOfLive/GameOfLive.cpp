a9c6c3001b400e383aab828a1033e125ceff2b1a
Slavic <43368065+SlayVic@users.noreply.github.com>
2021-04-25 20:17:41 +0300

    remove consol functions

--- GameOfLive/GameOfLive/GameOfLive.cpp
+++ GameOfLive/GameOfLive/GameOfLive.cpp
@@ -5,7 +5,6 @@
 #include <thread>
 #include <chrono>
 #include <SFML/Graphics.hpp>
-#include <windows.h>
 // #include <time.h>
 
 const clock_t simSpeed = 50;
@@ -32,22 +31,6 @@ void clearField(bool **field)
             field[i][j] = false;
         }
     }
-}
-
-void clear()
-{
-    COORD topLeft = {0, 0};
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_SCREEN_BUFFER_INFO screen;
-    DWORD written;
-
-    GetConsoleScreenBufferInfo(console, &screen);
-    FillConsoleOutputCharacterA(
-        console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
-    FillConsoleOutputAttribute(
-        console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
-        screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
-    SetConsoleCursorPosition(console, topLeft);
 }
 
 void nextLifeCycle(bool **field, sf::RenderWindow *window)
@@ -129,28 +112,6 @@ void nextLifeCycle(bool **field, sf::RenderWindow *window)
         newFieldReady = true;
     }
     return;
-}
-
-void consoleDraw(bool field[size][size])
-{
-    while (true)
-    {
-        while (!newFieldReady)
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        }
-        clear();
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
-                std::cout << (field[i][j] ? " #" : " .");
-            }
-            std::cout << "\n";
-        }
-        newFieldReady = false;
-        startChangingField = true;
-    }
 }
 
 void graphDraw(sf::RenderWindow *window, bool **field)
